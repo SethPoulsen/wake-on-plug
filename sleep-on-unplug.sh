6b97995a424485c9a1ec35c6d519d3e44912e88c
Seth Poulsen <poulsenseth@yahoo.com>
2024-10-13 14:37:17 -0600

    update file to read for Framework laptop

--- sleep-on-unplug.sh
+++ sleep-on-unplug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if power cable is unplugged
-if [ $(cat /sys/class/power_supply/AC/online) -eq 0 ]; then
+if [ $(cat /sys/class/power_supply/ACAD/online) -eq 0 ]; then
     # Trigger system sleep
     systemctl suspend
 fi
--- wake-on-plug.sh
+++ wake-on-plug.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if power cable is plugged in
-if [ $(cat /sys/class/power_supply/AC/online) -eq 1 ]; then
+if [ $(cat /sys/class/power_supply/ACAD/online) -eq 1 ]; then
     # Trigger system wake up
     systemctl start systemd-logind.service
 fi
